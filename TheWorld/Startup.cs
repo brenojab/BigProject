d4b10b531dbe7e443c3cb9c3b2b8a3be20e061a7
Breno Batista <brenojab@gmail.com>
2017-10-22 18:33:33 -0200

    - Projeto de estudo do Pluralsight sobre "ASP.NET Core, MVC 6, EF Core, e Angular".
    https://app.pluralsight.com/library/courses/aspdotnetcore-efcore-bootstrap-angular-web-app/table-of-contents

---
+++ TheWorld/Startup.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace TheWorld
+{
+  public class Startup
+  {
+    // This method gets called by the runtime. Use this method to add services to the container.
+    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+    public void ConfigureServices(IServiceCollection services)
+    {
+
+      // é preciso adicionar o MVC nas configurações para
+      // as páginas funcionarem
+      services.AddMvc();
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    {
+      //#if DEBUG
+      //app.UseDeveloperExceptionPage();
+      //#endif
+
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+
+
+      // A ordem faz diferença pois se trata da execução da Middleware.
+
+      // É isso aqui que faz utilizar o wwwroot ou as Views do ASP.Net
+      //app.UseDefaultFiles();
+
+      app.UseStaticFiles();
+
+      app.UseMvc(config =>
+      {
+        config.MapRoute(
+          name: "Default",
+          template: "{controller}/{action}/{id?}",
+          defaults: new { controller = "App", action = "Index" }
+          );
+      });
+      
+      //app.Run(async (context) =>
+      //{
+      //    await context.Response.WriteAsync("Hello World!");
+      //});
+    }
+  }
+}
---
+++ TheWorld/wwwroot/js/site.js
@@ -0,0 +1,32 @@
+﻿// site.js
+(function () {
+
+    var ele = $("#userName");
+    ele.text("Breno Batista");
+
+    var main = $("#main");
+    main.on("mouseenter", function () {
+        main.css("background-color", "#E6AC00");
+    });
+
+    main.on("mouseleave", function () {
+        main.css("background-color", "");
+    });
+
+  //$(".menu li a").on("click", function () {
+  //  alert($(this).text());
+  //  return false;
+  //});
+
+    var $sidebarAndWrapper = $("#sidebar,#wrapper");
+
+  $("#menuToggle").on("click", function () {
+    $sidebarAndWrapper.toggleClass("hide-sidebar");
+    if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+      $(this).text("Exibe Menu");
+    } else {
+      $(this).text("Oculta Menu");
+    }
+  });
+
+})();
